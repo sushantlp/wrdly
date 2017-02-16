b5e1227f65a7ac3e5fe6a8d30d2ffff98792c576
sushant singh chauhan <sushantsingh.1081@gmail.com>
2017-02-16 11:47:54 +0000

    Aboulte path differnent

--- public/src/scripts/config.router.js
+++ public/src/scripts/config.router.js
@@ -14,28 +14,32 @@
         $stateProvider
             .state('signin', {
                 url: '/signin',
-                templateUrl: 'src/template/signin.html',
+            //    templateUrl: 'src/template/signin.html',
+                templateUrl: 'template/signin.html',
                 controller: 'LoginController',
                 controllerAs: 'loginCtrl',
             })
 
             .state('signup', {
                 url: '/signup',
-                templateUrl: 'src/template/signup.html',
+            //    templateUrl: 'src/template/signup.html',
+                templateUrl: 'template/signup.html',
                 controller: 'SignUpController',
                 controllerAs: 'signupCtrl',
             })
 
             .state('lockme', {
                 url: '/lockme',
-                templateUrl: 'src/template/lockme.html',
+            //    templateUrl: 'src/template/lockme.html',
+                templateUrl: 'template/lockme.html',
                 controller: 'LockMeController',
                 controllerAs: 'lockmeCtrl',
             })
 
             .state('forgot-password', {
                 url: '/forgot-password',
-                templateUrl: 'src/template/forgot-password.html',
+            //    templateUrl: 'src/template/forgot-password.html',
+                templateUrl: 'template/forgot-password.html',
                 controller: 'ForgotPasswordController',
                 controllerAs: 'forgotCtrl',
             });
