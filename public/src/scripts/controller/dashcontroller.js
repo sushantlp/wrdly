0d289f88e591d9e21b5082764548bc28d914d264
sushantlp <sushantsingh.1081@gmail.com>
2017-03-22 01:13:19 +0530

    Dashboard Ui Started

--- public/src/scripts/controller/dashcontroller.js
+++ public/src/scripts/controller/dashcontroller.js
@@ -2,11 +2,22 @@
 
     'use strict';
     angular.module('wrdly')
-    .controller('DashboardController', DashboardController);
+    .controller('DashboardController', DashboardController)
+    .config(cardConfig);
 
     DashboardController.$inject = ['RestfullApi'];
+    cardConfig.$inject = ['$mdThemingProvider','$mdIconProvider'];
+
+    function cardConfig($mdThemingProvider,$mdIconProvider) {
+        $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+        $mdIconProvider.fontSet('md', 'material-icons');
+    }
 
     function DashboardController(RestfullApi) {
+        var dash = this;
 
+        dash.changeIconColor = function() {
+            //var element = document.getElementById('favorite').style.color = "blue";
+        };
     }
 })();
