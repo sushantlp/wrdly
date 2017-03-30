b7cb859d5212de602fdc957a05cdcdd992edd5e1
sushantlp <sushantsingh.1081@gmail.com>
2017-03-31 01:37:34 +0530

    Dashboard ribble effect

--- public/src/scripts/controller/dashcontroller.js
+++ public/src/scripts/controller/dashcontroller.js
@@ -5,7 +5,7 @@
     .controller('DashboardController', DashboardController)
     .config(cardConfig);
 
-    DashboardController.$inject = ['RestfullApi'];
+    DashboardController.$inject = ['RestfullApi','$mdDialog'];
     cardConfig.$inject = ['$mdThemingProvider','$mdIconProvider'];
 
     function cardConfig($mdThemingProvider,$mdIconProvider) {
@@ -13,41 +13,69 @@
         $mdIconProvider.fontSet('md', 'material-icons');
     }
 
-    function DashboardController(RestfullApi) {
+    function DashboardController(RestfullApi,$mdDialog) {
         var dash = this;
-
+        dash.change = 'grey';
         dash.content = [
             {
-                past: 'fish'
+                past: 'fish',
+                color: '#ff6686'
             },
             {
-                past: 'yellow'
+                past: 'yellow',
+                color: '#ffc555'
             },
             {
-                past: 'green'
+                past: 'green',
+                color: '#33DAA0'
             },
             {
-                past: 'blue'
+                past: 'blue',
+                color: '#21e8ea'
             },
             {
-                past: 'red'
+                past: 'red',
+                color: '#D52735'
             },
             {
-                past: 'ultra'
+                past: 'ultra',
+                color: '#7b53fc'
             },
             {
-                past: 'nice-green'
+                past: 'nice-green',
+                color: '#65A026'
             },
             {
                 past: 'orange',
+                color: '#ff6686'
             }
         ];
 
 
         // Execute When User Like or Dislike Thought
         dash.hitLike = function() {
-            dash.change = 'red';
+            dash.change = '#FF6666';
         }
 
+        dash.showPrompt = function() {
+            swal({
+                input: 'textarea',
+                showCancelButton: true,
+                inputPlaceholder: 'Your words here...',
+                inputAttributes: {
+    'maxlength': 200,
+    'autocapitalize': 'off',
+    'autocorrect': 'off'
+},
+                width: 800,
+                padding: 100,
+                html:
+                '<md-button class="md-raised md-accent btn-fw m-b-sm">Next</md-button>'
+            }).then(function (text) {
+                if (text) {
+                    swal(text)
+                }
+            })
+        };
     }
 })();
